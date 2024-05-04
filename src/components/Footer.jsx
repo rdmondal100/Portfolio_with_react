c9a4c193715b8ca3999e9137c8594b81eda30c38
Riday <rdmondal100@gmail.com>
2024-05-04 22:36:51 +0600

    fixes indentatios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
-import Socials from "./Socials"
+import Socials from "./Socials";
 
-const Footer = () => { 
-  return (
-    <footer className=" bg-footer-background py-12 lg:pb-2  border-solid  rounded-md">
-      <div className=" mx-auto my-3">
-        <div className="">
-          <Socials containerStyles='flex gap-x-6  justify-center' iconStyles='text-footer-foreground  text-[25px] md:text-[27px] xl:text-[29px] mb-4 ' />
-          <div className=" text-muted-foreground text-center">
-            Copyright &copy; Riday Mondal. All rights reserved.
-          </div>
-        </div>
-      </div>
+const Footer = () => {
+    return (
+        <footer className=' bg-footer-background py-12 lg:pb-2  border-solid  rounded-md'>
+            <div className=' mx-auto my-3'>
+                <div className=''>
+                    <Socials
+                        containerStyles='flex gap-x-6  justify-center'
+                        iconStyles='text-footer-foreground  text-[25px] md:text-[27px] xl:text-[29px] mb-4 '
+                    />
+                    <div className=' text-muted-foreground text-center'>
+                        Copyright &copy; Riday Mondal. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
