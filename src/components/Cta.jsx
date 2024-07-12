ea0475bfd5dfb596d596dd078fd7c7f967479527
Riday <rdmondal100@gmail.com>
2024-07-12 16:55:57 +0600

    change cta animation

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -9,7 +9,7 @@ const Cta = () => {
     <section className=" py-16 bg-accent rounded-t-xl">
       <div className="mx-auto pb-12 px-3">
         <div className="flex flex-col items-center">
-          <h2 className=" font-heading_font text-foreground text-[1.7rem] md:text-3xl xl:text-4xl mb-8 text-center  max-w-xl animate-live-link ">Looking for a developer to turn your ideas into reality? I'm here to help</h2>
+          <h2 className=" font-heading_font text-foreground text-[1.7rem] md:text-3xl xl:text-4xl mb-8 text-center  max-w-xl  text-green-600 animate-bounce">Looking for a developer to turn your ideas into reality? I'm here to help</h2>
             <Link to={'/contact'}>
             <Button className=' text-lg'>Contact me<GrSend className=" ml-2 text-lg"/></Button>
             </Link>
